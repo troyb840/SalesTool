4311911fb9ecbd714c45a46890adb7328652dbd0
Troy Brown <troyb840@gmail.com>
2017-04-09 20:13:36 -0400

    initial checkin of sales tool

---
+++ SalesTool/src/SalesData.java
@@ -0,0 +1,12 @@
+
+
+public class SalesData {
+   int data[] = {0, 4, 42};
+
+   public void display() {
+       System.out.println("TODO: Implement data display");
+   } //End display
+
+}  //End class SalesData
+
+  
